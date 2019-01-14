0eea3fa7ec173d3f683c78d8f15bb46c7356077f
Barney Boisvert <bboisvert@gmail.com>
2019-01-13 21:07:00 -0800

    convert to JSX

--- src/Tag.js
+++ src/Tag.js
@@ -3,21 +3,16 @@ import React from "react";
 class Tag extends React.Component {
     render() {
         let n = this.props.number;
-        return React.createElement("code", {
-            style: {
-                display: "inline-block",
-                padding: "1px 3px",
-                margin: "1px 5px 1px 0",
-                border: "1px solid #09f",
-                backgroundColor: "#f0f7ff",
-            },
-        }, [
-            this.props.tag + (
-                n != null && n !== 1
-                    ? (":" + n)
-                    : ""
-            ),
-        ]);
+        return <code style={{
+            display: "inline-block",
+            padding: "1px 3px",
+            margin: "1px 5px 1px 0",
+            border: "1px solid #09f",
+            backgroundColor: "#f0f7ff",
+        }}>
+            {this.props.tag}
+            {n != null && n !== 1 && (":" + n)}
+        </code>;
     }
 }
 
--- src/TagEditor.js
+++ src/TagEditor.js
@@ -19,40 +19,28 @@ const parse = tags =>
             };
         });
 
-const e = React.createElement;
-
 class TagEditor extends React.PureComponent {
 
     render() {
         const objs = parse(this.props.tags);
-        return e("div", null, [
-            e("textarea", {
-                key: "textarea",
-                rel: "from-react",
-                value: this.props.tags,
-                onChange: e => this.props.onChange(e.target.value),
-            }),
-            e("div", {
-                key: "editor",
-                style: {
-                    padding: "3px",
-                    border: "1px solid #eee",
-                },
-            }, objs.map(t => e(Tag, {
-                key: t.tag,
-                ...t,
-            }))),
-            e("pre", {
-                key: "pre",
-                style: {
-                    padding: "5px",
-                    border: "1px solid #ff0",
-                    backgroundColor: "#ffe",
-                }
-            }, [
-                JSON.stringify(objs, null, 3)
-            ])
-        ]);
+        return <div>
+            <textarea rel="from-react"
+                      value={this.props.tags}
+                      onChange={e => this.props.onChange(e.target.value)}/>
+            <div style={{
+                padding: "3px",
+                border: "1px solid #eee",
+            }}>
+                {objs.map(t => <Tag key={t.tag} {...t} />)}
+            </div>
+            <pre style={{
+                padding: "5px",
+                border: "1px solid #ff0",
+                backgroundColor: "#ffe",
+            }}>
+                {JSON.stringify(objs, null, 3)}
+            </pre>
+        </div>;
     }
 }
 
