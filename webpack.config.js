f9a8fa0530e746545d97e475fe2c742e7ff2e8a4
Barney Boisvert <bboisvert@gmail.com>
2019-01-13 21:37:24 -0800

    use a template for `index.html`, instead of editing in `dist`

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
     process.env.NODE_ENV = env.NODE_ENV;
@@ -11,6 +12,11 @@ module.exports = env => {
             libraryTarget: 'umd',
             path: path.resolve(__dirname, 'dist'),
         },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html'
+            })
+        ],
         module: {
             rules: [
                 {
