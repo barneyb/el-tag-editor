7ab566811df55c14ddbf113cb41d69d1b56b8f7b
Barney Boisvert <bboisvert@gmail.com>
2019-01-13 20:14:46 -0800

    little parsing and pretty rendering

---
+++ src/Tag.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Tag extends React.Component {
+    render() {
+        let n = this.props.number;
+        return React.createElement("code", {
+            style: {
+                display: "inline-block",
+                padding: "1px 3px",
+                margin: "1px 5px 1px 0",
+                border: "1px solid #09f",
+                backgroundColor: "#f0f7ff",
+            },
+        }, [
+            this.props.tag + (
+                n != null && n !== 1
+                    ? (":" + n)
+                    : ""
+            ),
+        ]);
+    }
+}
+
+export default Tag;
--- src/TagEditor.js
+++ src/TagEditor.js
@@ -1,12 +1,58 @@
 import React from "react";
+import Tag from "./Tag";
 
-class TagEditor extends React.Component {
-    render() {
-        return React.createElement('textarea', {
-            rel: "from-react",
-            value: this.props.tags,
-            onChange: e => this.props.onChange(e.target.value),
+const parse = tags =>
+    tags.trim().split(",")
+        .map(t => t.trim())
+        .filter(t => t.length > 0)
+        .map(t => {
+            if (t.indexOf(":") < 0) {
+                return {
+                    tag: t,
+                };
+            }
+            const parts = t.split(":");
+            // blindly assume two parts :)
+            return {
+                tag: parts[0],
+                number: parseFloat(parts[1]),
+            };
         });
+
+const e = React.createElement;
+
+class TagEditor extends React.PureComponent {
+
+    render() {
+        const objs = parse(this.props.tags);
+        return e("div", null, [
+            e("textarea", {
+                key: "textarea",
+                rel: "from-react",
+                value: this.props.tags,
+                onChange: e => this.props.onChange(e.target.value),
+            }),
+            e("div", {
+                key: "editor",
+                style: {
+                    padding: "3px",
+                    border: "1px solid #eee",
+                },
+            }, objs.map(t => e(Tag, {
+                key: t.tag,
+                ...t,
+            }))),
+            e("pre", {
+                key: "pre",
+                style: {
+                    padding: "5px",
+                    border: "1px solid #ff0",
+                    backgroundColor: "#ffe",
+                }
+            }, [
+                JSON.stringify(objs, null, 3)
+            ])
+        ]);
     }
 }
 
