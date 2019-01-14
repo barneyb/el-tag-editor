e2e70ad3fd0d99da54dcb824f50c28ce5efa64eb
Barney Boisvert <bboisvert@gmail.com>
2019-01-13 20:58:02 -0800

    set up CRA's babel preset, mostly for JSX, but ES6 for free

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,38 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/TagEditor.js',
-    output: {
-        filename: 'TagEditor.js',
-        library: 'TagEditor',
-        libraryTarget: 'umd',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React',
+module.exports = env => {
+    process.env.NODE_ENV = env.NODE_ENV;
+    const config = {
+        mode: 'production',
+        entry: './src/TagEditor.js',
+        output: {
+            filename: 'TagEditor.js',
+            library: 'TagEditor',
+            libraryTarget: 'umd',
+            path: path.resolve(__dirname, 'dist'),
         },
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },
+            ],
+        },
+        externals: {
+            react: {
+                commonjs: 'react',
+                commonjs2: 'react',
+                amd: 'react',
+                root: 'React',
+            },
+        },
+    };
+    if (env.NODE_ENV === 'development') {
+        config.mode = 'development';
+        config.devtool = 'inline-source-map';
+        config.watch = true;
+    }
+    return config;
 };
