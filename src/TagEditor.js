046985baa22cfea6fd9984891e5d759156203841
Barney Boisvert <bboisvert@gmail.com>
2019-01-13 17:49:37 -0800

    show tag state in-app, rather than in the console

--- src/TagEditor.js
+++ src/TagEditor.js
@@ -3,6 +3,7 @@ import React from "react";
 class TagEditor extends React.Component {
     render() {
         return React.createElement('textarea', {
+            rel: "from-react",
             value: this.props.tags,
             onChange: e => this.props.onChange(e.target.value),
         });
