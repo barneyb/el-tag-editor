0adc9979ea5a5653ea3b1198e3ed866d0b8b800d
barneyb <bboisvert@gmail.com>
2019-01-18 18:21:52 -0800

    don't sanitize tags

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -5,13 +5,7 @@ class Tag extends React.PureComponent {
 
     constructor(props) {
         super(props);
-        this.sanitize = this.sanitize.bind(this);
         this.doKeyDown = this.doKeyDown.bind(this);
-    }
-
-    // noinspection JSMethodCanBeStatic
-    sanitize(val) {
-        return val.trim();
     }
 
     doKeyDown(e) {
@@ -30,7 +24,6 @@ class Tag extends React.PureComponent {
         } = this.props;
         return <EditInPlace className="EditInPlace tag"
                             value={tag}
-                            sanitize={this.sanitize}
                             onCommit={onRename}
                             onKeyDown={this.doKeyDown}
         />;
